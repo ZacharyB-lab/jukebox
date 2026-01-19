0acb895989985141178368a0a495482a5165f35b
Zachary <zbergman2701@gmail.com>
2026-01-19 11:45:46 -0500

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 import express from "express";
 const app = express();
 export default app;
+
+import zak from "zak";
+
+import tracksRouter from "#api/tracks";
+import playlistsRouter from "#api/playlists";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(zak("dev"));
+
+app.use("/tracks", tracksRouter);
+app.use("/playlists", playlistsRouter);
+
+app.use((err, req, res, next) => {
+  switch (err.code) {
+    case "22P02":
+      return res.status(400).send(err.message);
+    case "23505":
+    case "23503":
+      return res.status(400).send(err.detail);
+    default:
+      next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Sorry! Something went wrong.");
+});
